703b1cf01c04e13bc790557aac582ef7c1a91172
idoburnish <doyun7433@naver.com>
2021-12-12 01:15:17 +0900

    firebase setting

--- app/src/main/java/com/example/healthycollege/LoginActivity.kt
+++ app/src/main/java/com/example/healthycollege/LoginActivity.kt
@@ -2,10 +2,51 @@ package com.example.healthycollege
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.healthycollege.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        
+        auth = Firebase.auth
+
+        binding.signInBtn.setOnClickListener {
+            var email = binding.loginEmail.text.toString()
+            var password = binding.loginPassword.text.toString()
+            createAccount(email, password)
+        }
+    }
+    
+    private fun createAccount(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            Log.d("확인", email)
+            Log.d("확인", password)
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish() // 가입창 종료
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
