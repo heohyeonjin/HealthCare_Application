7825b57f0669729fa931ca4d35f72d074c5a692d
heohyeonjin <hyeonjin4870@naver.com>
2021-12-19 01:39:22 +0900

    modify friendApi

--- app/src/main/java/com/example/healthycollege/ui/navigation/friend/FriendListFragment.kt
+++ app/src/main/java/com/example/healthycollege/ui/navigation/friend/FriendListFragment.kt
@@ -1,9 +1,11 @@
 package com.example.healthycollege.ui.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,39 +19,47 @@ import com.example.healthycollege.databinding.FragmentFriendlistBinding
 
 class FriendListFragment : Fragment() {
 
-    companion object{
-        fun newInstance() : FriendListFragment{
+    companion object {
+        fun newInstance(): FriendListFragment {
             return FriendListFragment()
         }
     }
 
     var friendList = ArrayList<Friend>()
-    private lateinit var friendAdapter : FriendListAdapter
+    private lateinit var friendAdapter: FriendListAdapter
     private lateinit var friendRecyclerView: RecyclerView
-    private lateinit var binding:FragmentFriendlistBinding
+    private lateinit var binding: FragmentFriendlistBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_friendlist,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_friendlist, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view:View,savedInstanceState: Bundle?){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         friendAdapter = FriendListAdapter(friendList)
         friendRecyclerView = view.findViewById(R.id.fragment_friendlist_recyclerview)
         friendRecyclerView.adapter = friendAdapter
-        friendRecyclerView.layoutManager = LinearLayoutManager(activity,RecyclerView.VERTICAL,false)
+        friendRecyclerView.layoutManager =
+            LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         friendRecyclerView.setHasFixedSize(true)
 
-        FriendApiService.instance.getFriendList(){
-            for(friend in it){
+        FriendApiService.instance.getFriendList() {
+            for (friend in it) {
                 friendAdapter.setFriend(friend)
             }
         }
+//        friendAdapter.setItemClickListener(object: FriendListAdapter.OnItemClickListener{
+//            override fun onClick(v: View, position: Int) {
+//                val friendName = friendList[position].friendName;
+//                Toast.makeText(context,friendName,Toast.LENGTH_LONG);
+//            }
+//
+//        })
     }
 }
